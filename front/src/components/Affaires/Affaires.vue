540b750a388e309a18038c708e2c6d93296e71cd
Chokri Koussa <cko@arxit.lan>
2020-01-30 17:41:37 +0100

    Structure Vue.js (config, routing, authentification ...)

---
+++ front/src/components/Affaires/Affaires.vue
@@ -0,0 +1,13 @@
+<style src="./affaires.css" scoped></style>
+<template src="./affaires.html"></template>
+
+
+<script>
+export default {
+  name: 'Affaires',
+  props: {
+    msg: String
+  }
+}
+</script>
+
