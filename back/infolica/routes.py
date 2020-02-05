58f2d4f5ff44af8cc4ba3a5a30202467fb908ad1
rufener <marc.rufener@gmail.com>
2020-02-05 11:48:45 +0100

    put emolument_facture

--- back/infolica/routes.py
+++ back/infolica/routes.py
@@ -78,6 +78,6 @@ def includeme(config):
     #Documents affaire
     config.add_route('affaire_documents_by_affaire_id','/infolica/api/affaire_documents/{id}')
     #Emoluments facture
-    config.add_route('facture_emoluments','/infolica/api/facture_emoluments')
-    config.add_route('facture_emoluments_s','/infolica/api/facture_emoluments/')
+    config.add_route('emolument_facture','/infolica/api/emolument_facture')
+    config.add_route('emolument_facture_s','/infolica/api/emolument_facture/')
     config.add_route('facture_emoluments_by_facture_id','/infolica/api/facture_emoluments/{id}')
--- back/infolica/views/facture.py
+++ back/infolica/views/facture.py
@@ -141,21 +141,17 @@ def facture_emoluments_view(request):
         return Response(db_err_msg, content_type='text/plain', status=500)
 
 
-""" Add new facture_emolument"""
-@view_config(route_name='facture_emoluments', request_method='POST', renderer='json')
-@view_config(route_name='facture_emoluments_s', request_method='POST', renderer='json')
-def facture_emoluments_new_view(request):
+""" Add new emolument_facture"""
+@view_config(route_name='emolument_facture', request_method='POST', renderer='json')
+@view_config(route_name='emolument_facture_s', request_method='POST', renderer='json')
+def emolument_facture_new_view(request):
 
-    model = models.EmolumentFacture()
-    model = Utils.set_model_record(model, request.params)
-    # récupère le prix unitaire de l'élément
-    prix_unitaire = request.dbsession.query(models.TableauEmoluments).filter(models.TableauEmoluments.id==model.emolument_id).first().montant
-    # calcule le montant des éléments sélectionnés
-    model.montant = int(model.nombre) * float(prix_unitaire) * float(model.facteur_correctif)
+    record = models.EmolumentFacture()
+    record = Utils.set_model_record(record, request.params)
     
     try:
         with transaction.manager:
-            request.dbsession.add(model)
+            request.dbsession.add(record)
             transaction.commit()
             return Utils.get_data_save_response(Constant.SUCCESS_SAVE.format(models.EmolumentFacture.__tablename__))
 
@@ -163,41 +159,31 @@ def facture_emoluments_new_view(request):
         log.error(e)
         return Response(db_err_msg, content_type='text/plain', status=500)
 
-    
 
+""" Update emolument_facture"""
+@view_config(route_name='emolument_facture', request_method='PUT', renderer='json')
+@view_config(route_name='emolument_facture_s', request_method='PUT', renderer='json')
+def emolument_facture_update_view(request):
+    emolument_facture_id = request.params['id'] if 'id' in request.params else None
 
-# """ Update facture"""
-# @view_config(route_name='factures', request_method='PUT', renderer='json')
-# @view_config(route_name='factures_s', request_method='PUT', renderer='json')
-# def factures_update_view(request):
-#     try:
+    # Get the facture
+    record = request.dbsession.query(models.EmolumentFacture).filter(
+        models.EmolumentFacture.id == emolument_facture_id).first()
 
-#         # id_facture
-#         id_facture = None
+    if not record:
+        raise CustomError(
+            CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.EmolumentFacture.__tablename__, emolument_facture_id))
 
-#         if 'id_facture' in request.params:
-#             id_facture = request.params['id_facture']
+    record = Utils.set_model_record(record, request.params)
 
-#         # Get the facture
-#         facture_record = request.dbsession.query(models.Facture).filter(
-#             models.Facture.id == id_facture).first()
+    try:
+        with transaction.manager:
+            transaction.commit()
+            return Utils.get_data_save_response(Constant.SUCCESS_SAVE.format(models.EmolumentFacture.__tablename__))
 
-#         if not facture_record:
-#             raise CustomError(
-#                 CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Facture.__tablename__, id_facture))
-
-#         facture_record = Utils.set_model_record(facture_record, request.params)
-
-#         with transaction.manager:
-
-#             # Commit transaction
-#             transaction.commit()
-
-#     except DBAPIError as e:
-#         log.error(e)
-#         return Response(db_err_msg, content_type='text/plain', status=500)
-
-#     return Utils.get_data_save_response(Constant.SUCCESS_SAVE.format(models.Facture.__tablename__))
+    except DBAPIError as e:
+        log.error(e)
+        return Response(db_err_msg, content_type='text/plain', status=500)
 
 
 # """ Delete facture"""
