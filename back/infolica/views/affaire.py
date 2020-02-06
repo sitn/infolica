df13219a3bf345f85c68631763c9c85e800b123e
rufener <marc.rufener@gmail.com>
2020-02-06 14:26:49 +0100

    delete envoi

--- back/infolica/views/affaire.py
+++ back/infolica/views/affaire.py
@@ -28,7 +28,6 @@ def affaires_view(request):
         query = request.dbsession.query(models.VAffaire).all()
         return Utils.serialize_many(query)
     except DBAPIError as e:
-        print(e)
         log.error(e)
         return Response(db_err_msg, content_type='text/plain', status=500)
 
@@ -351,10 +350,9 @@ def preavis_new_view(request):
 @view_config(route_name='preavis_s', request_method='PUT', renderer='json')
 def preavis_update_view(request):
     preavis_id = request.params['id'] if 'id' in request.params else None
-    print(preavis_id)
     record = request.dbsession.query(models.Preavis).filter(
         models.Preavis.id == preavis_id).first()
-    print("toto")
+
     if not record:
         raise CustomError(
             CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Preavis.__tablename__, preavis_id))
@@ -458,13 +456,13 @@ def envois_new_view(request):
 @view_config(route_name='envois_s', request_method='PUT', renderer='json')
 def envois_update_view(request):
     
-    envoi_id = request.params['id'] if 'id' in request.params else None
+    record_id = request.params['id'] if 'id' in request.params else None
     record = request.dbsession.query(models.Envoi).filter(
-        models.Envoi.id == envoi_id).first()
+        models.Envoi.id == record_id).first()
     
     if not record:
         raise CustomError(
-            CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Envoi.__tablename__, preavis_id))
+            CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Envoi.__tablename__, record_id))
 
     record = Utils.set_model_record(record, request.params)
 
@@ -478,28 +476,27 @@ def envois_update_view(request):
         return Response(db_err_msg, content_type='text/plain', status=500)
 
 
-# """ DELETE preavis affaire"""
-# @view_config(route_name='preavis_by_id', request_method='DELETE', renderer='json')
-# def preavis_delete_view(request):
-#     preavis_id = request.matchdict['id']
+""" DELETE envois"""
+@view_config(route_name='envois_by_id', request_method='DELETE', renderer='json')
+def envois_delete_view(request):
+    record_id = request.matchdict['id']
 
-#     record = request.dbsession.query(models.Preavis).filter(
-#         models.Preavis.id == preavis_id).first()
+    record = request.dbsession.query(models.Envoi).filter(
+        models.Envoi.id == record_id).first()
 
-#     if not record:
-#         raise CustomError(
-#             CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Preavis.__tablename__, preavis_id))
+    if not record:
+        raise CustomError(
+            CustomError.RECORD_WITH_ID_NOT_FOUND.format(models.Envoi.__tablename__, record_id))
 
-#     try:
-#         with transaction.manager:
-#             request.dbsession.delete(record)
-#             # Commit transaction
-#             transaction.commit()
-#             return Utils.get_data_save_response(Constant.SUCCESS_DELETE.format(models.Preavis.__tablename__))
+    try:
+        with transaction.manager:
+            request.dbsession.delete(record)
+            transaction.commit()
+            return Utils.get_data_save_response(Constant.SUCCESS_DELETE.format(models.Envoi.__tablename__))
 
-#     except DBAPIError as e:
-#         log.error(e)
-#         return Response(db_err_msg, content_type='text/plain', status=500)
+    except DBAPIError as e:
+        log.error(e)
+        return Response(db_err_msg, content_type='text/plain', status=500)
 
 
 
