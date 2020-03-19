055e9b86299bedd01396ae744e47bd6b5a0ac64d
Marc Rufener <marc.rufener@gmail.com>
2020-03-19 08:12:03 +0100

    minor changes in views sql

--- back/sql/vue_affaires_preavis.sql
+++ back/sql/vue_affaires_preavis.sql
@@ -2,5 +2,5 @@ create view infolica.v_affaires_preavis as
 select pr.affaire_id as affaire_id, ser.service as service, prt.nom as preavis , pr.date_demande as date_demande, pr.date_reponse as date_reponse
 from infolica.preavis as pr, infolica.service as ser, infolica.preavis_type as prt
 where pr.service_id = ser.id
-and pr.preavis_id = prt.id
+and pr.preavis_type_id = prt.id
 order by affaire_id 
