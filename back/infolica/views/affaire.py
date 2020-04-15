5346f5a1fc87f7b1cf8fdc2b074e4f86145290d4
rufener <marc.rufener@gmail.com>
2020-04-15 08:44:52 +0200

    correction filtre des données backend

--- back/infolica/views/affaire.py
+++ back/infolica/views/affaire.py
@@ -59,11 +59,15 @@ def affaires_search_view(request):
 @view_config(route_name='types_affaires', request_method='GET', renderer='json')
 @view_config(route_name='types_affaires_s', request_method='GET', renderer='json')
 def types_affaires_view(request):
-  query = request.dbsession.query(models.AffaireType).all()
-  types_affaires = Utils.serialize_many(query)
+  records = request.dbsession.query(models.AffaireType).all()
+  types_affaires = list()
 
   # Supprimer type d'affaire "NE PLUS UTILISER"
-  types_affaires.pop(5)
+  for type_i in records:
+      if not "NE PLUS UTILISER" in type_i.nom:
+          types_affaires.append(type_i)
+
+  types_affaires = Utils.serialize_many(types_affaires)
   return types_affaires
 
 """ Add new affaire"""
