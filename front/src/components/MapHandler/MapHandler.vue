8e9fe624b3fc420aadbc93e8d8af38399ad794de
Chokri Koussa <cko@arxit.com>
2020-06-26 19:19:01 +0200

    Delete commit

--- front/src/components/MapHandler/MapHandler.vue
+++ front/src/components/MapHandler/MapHandler.vue
@@ -226,8 +226,6 @@ export default {
       this.vectorSource.addFeature(marker);
 
       if(zoom){
-        //var size = this.map.getSize();
-        //this.view.centerOn(marker.getGeometry().getCoordinates(), size, [570, 500]);
         this.view.setCenter([x, y]);
         this.view.setZoom(process.env.VUE_APP_MAP_DEFAULT_AFFAIRE_ZOOM);
       }
