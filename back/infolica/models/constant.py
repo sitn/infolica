3317f9bc592f92ec8a83d12366fffcdc44a8ce8c
Marc Rufener <marc.rufener@gmail.com>
2020-07-03 15:21:49 +0200

    clean

--- back/infolica/models/constant.py
+++ back/infolica/models/constant.py
@@ -1,9 +1,4 @@
 class Constant(object):
-
-    # Path affaire directory
-    AFFAIRE_DIRECTORY = r"C:\infolica_project\affaire_technique"
-
-
     # Data save success
     SUCCESS_SAVE = 'Data successfully POSTED in {}'
     SUCCESS_DELETE = 'Data successfully DELETED from {}'
