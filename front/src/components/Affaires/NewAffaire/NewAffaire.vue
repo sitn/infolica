7145c545660fb2163d8aa40ccd3db442429c0a92
Marc Rufener <marc.rufener@gmail.com>
2020-08-14 15:04:58 +0200

    fix get affaire_numeros

--- front/src/components/Affaires/NewAffaire/NewAffaire.vue
+++ front/src/components/Affaires/NewAffaire/NewAffaire.vue
@@ -944,7 +944,7 @@ export default {
       this.$http
       .get(
         process.env.VUE_APP_API_URL +
-          process.env.VUE_APP_AFFAIRE_NUMEROS_ENDPOINT +
+          process.env.VUE_APP_AFFAIRE_NUMEROS_ENDPOINT + "/" +
           this.form.affaire_base_id + "?affaire_numero_actif=true",
         {
           withCredentials: true,
