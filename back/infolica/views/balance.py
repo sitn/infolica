e721fac17b8fdde6800151b6b5edc5542cf99dc8
Marc Rufener <marc.rufener@gmail.com>
2021-01-22 16:25:46 +0100

    balance (work in progress)

--- back/infolica/views/balance.py
+++ back/infolica/views/balance.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*--
 from pyramid.view import view_config
 import pyramid.httpexceptions as exc
+from sqlalchemy import and_
 
 from infolica.exceptions.custom_error import CustomError
-from infolica.models.models import Affaire, GeosBalance
+from infolica.models.models import Affaire, GeosBalance, Numero
 from infolica.scripts.utils import Utils
 from infolica.scripts.mailer import send_mail
 
@@ -54,3 +55,45 @@ def balance_view(request):
     if not mutation_name:
         raise CustomError(
             CustomError.RECORD_WITH_ID_NOT_FOUND.format(GeosBalance.__tablename__, mutation_name))
+
+    query = request.dbsession.query(GeosBalance).filter(GeosBalance.mutation == mutation_name).all()
+
+    return Utils.serialize_many(query)
+
+
+@view_config(route_name='balance_check_existing_oldBF', request_method='POST', renderer='json')
+def balance_check_existing_oldBF_new_view(request):
+    """
+    Check if oldBF already exist in DB and create it otherwise
+    """
+    # Check connected
+    if not Utils.has_permission(request, request.registry.settings['affaire_numero_edition']):
+        raise exc.HTTPForbidden()
+    
+    oldBF = json.loads(request.params['oldBF'])
+
+    # Control existance of each oldBF
+    numero_obj = []
+    for bf in oldBF:
+        bf_cadastre_id, bf_numero = bf.split("_")
+        query = request.dbsession.query(Numero).filter(and_(
+            Numero.cadastre_id == bf_cadastre_id,
+            Numero.numero == bf_numero
+        )).first()
+
+        # Create number if it doesn't exist
+        if not query:
+            numero = Numero(
+                cadastre_id = bf_cadastre_id,
+                type_id = request.registry.settings['numero_type_bf_id'],
+                numero = bf_numero,
+                etat_id = request.registry.settings['numero_vigueur_id']
+            )
+            
+            request.dbsession.flush()
+            numero_obj.append(numero)
+
+        else:
+            numero_obj.append(query)
+
+    return Utils.serialize_many(numero_obj)
