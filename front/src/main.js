38074d5e30a536c233bff112d12563efadf20760
Marc Rufener <marc.rufener@gmail.com>
2021-02-03 16:14:08 +0100

    add ';'

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-import App from './App.vue'
+import Vue from 'vue';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 //Vue material
 import VueMaterial from 'vue-material';
@@ -22,24 +22,24 @@ proj4.defs('EPSG:2056',
 '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333'
 + ' +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel '
 + '+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
-register(proj4)
+register(proj4);
 
 Vue.config.productionTip = false;
 
-Vue.use(VueMaterial)
-Vue.use(VueRouter)
-Vue.use(VueAxios, axios)
+Vue.use(VueMaterial);
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
 
 Vue.use(VueMoment, {
   moment,
-})
+});
 
 // Vue.material.locale.dateFormat = 'DD.MM.YYYY'
 Vue.filter('formatDate', function(value) {
   if (value) {
     return moment(String(value)).format(process.env.VUE_APP_DATEFORMAT_CLIENT)
   }
-})
+});
 
 const router = new VueRouter({
   mode: 'history',
@@ -53,7 +53,8 @@ router.beforeEach((to, from, next) => {
   else{
     next();
   }      
-})
+});
+
 
 new Vue({
   router,
