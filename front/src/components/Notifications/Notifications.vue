00f2c4d030381dc9549e2fe246b392f41b0be196
Marc Rufener <marc.rufener@gmail.com>
2021-04-22 15:18:51 +0200

    update error messages from back in front

--- front/src/components/Notifications/Notifications.vue
+++ front/src/components/Notifications/Notifications.vue
@@ -9,7 +9,8 @@ export default {
   data: () => ({
     msg: String,
     showSnackbar: false,
-    isError: false
+    isError: false,
+    duration: 5000 //ms
   }),
 
   mounted: function(){
@@ -17,14 +18,16 @@ export default {
     this.$root.$on('ShowMessage', (msg) =>{
       this.msg = msg;
       this.isError = false;
-      this.showSnackbar = true;      
+      this.showSnackbar = true;
+      this.duration = 5000;
     });
 
     //Show Error
     this.$root.$on('ShowError', (msg) =>{
       this.msg = msg;
       this.isError = true;
-      this.showSnackbar = true;      
+      this.showSnackbar = true;
+      this.duration = 10000;
     });
   }
 }
