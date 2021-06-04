7efd3a3c0f348b0210cc23fbbf15ada638d2a144
Marc Rufener <marc.rufener@gmail.com>
2021-06-04 16:40:57 +0200

    init empty emoluments component

---
+++ front/src/components/Affaires/Facturation/Infobulle/Emoluments.vue
@@ -0,0 +1,29 @@
+<style src="./emoluments.css" scoped></style>
+<template src="./emoluments.html"></template>
+
+
+<script>
+import { checkPermission } from '@/services/helper'
+
+
+export default {
+  name: 'Emoluments',
+  props: {
+    
+  },
+  data: function () {
+      return {
+          
+      }
+  },
+
+  methods:{
+
+  },
+
+  mounted: function(){
+     
+  }
+}
+</script>
+
