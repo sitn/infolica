3f18196eb8a94e161140b4010962f98044b6a45d
Marc Rufener <marc.rufener@gmail.com>
2022-02-11 12:49:01 +0100

    new formular of ctrl mpd

---
+++ back/infolica/alembic/versions/20220211_74b698ab82b0.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 74b698ab82b0
+Revises: b9a475553f12, 595d5ae348fd
+Create Date: 2022-02-11 09:19:03.157252
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '74b698ab82b0'
+down_revision = ('b9a475553f12', '595d5ae348fd')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
