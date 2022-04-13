f961592f59bdc8001616becf36df1b40800118b0
Marc Rufener <marc.rufener@gmail.com>
2022-04-13 12:31:14 +0200

    add map

--- front/src/components/Preavis/PreavisEdit.vue
+++ front/src/components/Preavis/PreavisEdit.vue
@@ -10,7 +10,25 @@ import PreavisEditDecision from "@/components/Preavis/PreavisEditDecision.vue";
 import { handleException } from "@/services/exceptionsHandler";
 
 import { Drag, Drop } from 'vue-drag-drop';
-// import moment from "moment";
+
+import "ol/ol.css";
+import {defaults as defaultInteractions, Snap, Modify, MouseWheelZoom} from 'ol/interaction';
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import {WMTSCapabilities} from "ol/format";
+import WMTS from "ol/source/WMTS";
+import {optionsFromCapabilities} from "ol/source/WMTS";
+import proj4 from "proj4";
+import {register as proj_register} from "ol/proj/proj4";
+import {Projection} from "ol/proj";
+import { Style, Circle, Fill, Stroke, RegularShape } from "ol/style";
+import GeoJSON from 'ol/format/GeoJSON';
+import {platformModifierKeyOnly} from 'ol/events/condition';
 
 export default {
   name: "PreavisAffaire",
@@ -29,6 +47,15 @@ export default {
       documents: [{}],
       droppedFiles: [],
       mapLoaded: false,
+      affaire_data: {},
+      map: null,
+      center: { x: null, y: null },
+      view: null,
+      vectorLayer: null,
+      vectorSource: null,
+      graphicsLayer: null,
+      graphicsLayerSource: null,
+      markerStyle: null,
     };
   },
 
@@ -55,26 +82,6 @@ export default {
           reject(err);
         });
       })
-    },
-
-    /**
-     * Show map
-     */
-    showMap() {
-      if(this.$refs && this.$refs.mapHandler && !this.mapLoaded){
-        this.center = {
-          x: this.affaire.coord_e,
-          y: this.affaire.coord_n
-        };
-        this.$refs.mapHandler.initMap(
-          this.center,
-          process.env.VUE_APP_MAP_DEFAULT_AFFAIRE_ZOOM
-        );
-        this.$refs.mapHandler.addMarker(this.center.x, this.center.y);
-        this.$refs.mapHandler.modify.setActive(false);
-        this.$refs.mapHandler.snap.setActive(false);
-        this.mapLoaded = true;
-      }
     },
 
     /**
@@ -174,14 +181,185 @@ export default {
       } else {
         this.$router.push({name: 'Preavis'});
       }
-    }
+    },
 
+    /**
+     * Init map
+     */
+    initMap: function(center, zoom) {
+
+      this._crs = process.env.VUE_APP_MAP_PROJECTION;
+      proj4.defs(this._crs,
+       '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333'
+       + ' +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel '
+       + '+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
+      const _extent = [2420000, 1030000, 2900000, 1360000];
+      proj_register(proj4);
+
+      //Init marker style
+      this.initMarkerStyle();
+
+      // Init GeoJSON reader
+      this.geojsonFormat = new GeoJSON();
+
+      // WMTS
+      this.wmtsLayer = new TileLayer();
+
+      // Vector layer
+      this.vectorSource = new VectorSource();
+      this.vectorLayer = new VectorLayer({
+        source: this.vectorSource
+      });
+
+      this.initWmtsSource();
+
+      // Graphics layer
+      this.initGraphicsLayer();
+
+      // Map layers
+      const layers = [this.wmtsLayer, this.vectorLayer, this.graphicsLayer];
+
+
+      const _projection = new Projection({
+        code: this._crs,
+        extent: _extent,
+      });
+
+      this.view = new View({
+        projection: _projection,
+        center: center
+          ? [center.x, center.y]
+          : process.env.VUE_APP_MAP_DEFAULT_CENTER,
+        zoom: zoom,
+        minZoom: 0,
+        maxZoom: 17
+        });
+      if (!this.map) {
+
+        const interactions = new defaultInteractions({
+          altShiftDragRotate: false,
+          pinchRotate: false,
+          mouseWheelZoom: false
+        });
+    
+        this.map = new Map({
+          layers: layers,
+          target: document.getElementById("mapDiv"),
+          view: this.view,
+          interactions: interactions
+        });
+
+        let mouseWheelInt = new MouseWheelZoom();
+        this.map.addInteraction(mouseWheelInt);
+
+        this.map.on('wheel', function(evt) {
+          mouseWheelInt.setActive(platformModifierKeyOnly(evt));
+        });
+
+        this.modify = new Modify({
+          source: this.vectorSource
+        });
+
+        this.snap = new Snap({
+          source: this.vectorSource
+        });
+
+      } else {
+        this.map
+          .getView()
+          .setCenter(
+            center
+              ? [center.x, center.y]
+              : process.env.VUE_APP_MAP_DEFAULT_CENTER
+          );
+      }    
+    },
+
+    /**
+     * Init marker style
+     */
+    initMarkerStyle: function() {
+      this.markerStyle = new Style({
+        image: new Circle({
+          radius: 7,
+          fill: new Fill({ color: "yellow" }),
+          stroke: new Stroke({
+            color: [0, 0, 0],
+            width: 2
+          })
+        })
+      });
+    },
+
+    /**
+     * Init graphics layer
+     */
+    initGraphicsLayer: function(){
+      this.graphicsLayerSource = new VectorSource();
+      this.graphicsLayer = new VectorLayer({
+        source: this.graphicsLayerSource,
+        style: new Style({
+          fill: new Fill({ color: '#b3ffff' }),
+          stroke: new Stroke({ color: 'red', width: 8 }),
+          image: new RegularShape({
+            fill: new Fill({ color: 'yellow' }),
+            stroke: new Stroke({ color: 'red', width: 4 }),
+            points: 4,
+            radius: 10,
+            radius2: 0,
+            angle: Math.PI / 4,
+          }),
+        }),
+        opacity: 0.5
+      });
+    },
+
+    initWmtsSource: function() {
+      this.$http
+          .get(
+            process.env.VUE_APP_WMTS_GETCAPABILITIES_URL
+          )
+          .then(response => {
+            const parser = new WMTSCapabilities();
+            const result = parser.read(response.data);
+            const WMTSoptions = optionsFromCapabilities(
+              result,{
+                layer: process.env.VUE_APP_WMTS_LAYER,
+                matrixSet: this._crs
+              }
+            )
+            const source = new WMTS(WMTSoptions);
+            this.wmtsLayer.setSource(source);
+         });
+
+    },
+    
+    /**
+     * Add marker
+     */
+    addMarker: function(x, y, zoom) {
+      this.vectorSource.clear();
+      const marker = new Feature({
+        geometry: new Point([x, y])
+      });
+      marker.setStyle(this.markerStyle);
+      this.vectorSource.addFeature(marker);
+
+      if(zoom){
+        this.view.setCenter([x, y]);
+        this.view.setZoom(process.env.VUE_APP_MAP_DEFAULT_AFFAIRE_ZOOM);
+      }
+    },
   },
 
   mounted: function() {
     this.getAffaire().then(() => {
-      this.showMap();
       this.getDocuments();
+
+      // map
+      let center = {x: this.affaire.coord_e, y: this.affaire.coord_n}
+      this.initMap(center, process.env.VUE_APP_MAP_DEFAULT_AFFAIRE_ZOOM);
+      this.addMarker(center.x, center.y);
     });
   }
 };
