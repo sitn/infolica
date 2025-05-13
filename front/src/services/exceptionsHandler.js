a4fc03148213c5e05707d0cc18da739a8b3f668f
Marc Rufener <marc.rufener@gmail.com>
2025-05-13 08:51:09 +0200

    update notifications

--- front/src/services/exceptionsHandler.js
+++ front/src/services/exceptionsHandler.js
@@ -5,10 +5,10 @@
 export const handleException = function (error, component) {
     //Error code
     let code = error && error.response && error.response.status || 500;
-    
+
     //No error but no content response
     if(error && error.status === 204){
-        component.$root.$emit("ShowError", "Aucune donnée trouvée");   
+        component.$root.$emit("ShowError", "Aucune donnée trouvée");
     }
     //Not authorized
     else if(code === 403){
@@ -23,20 +23,15 @@ export const handleException = function (error, component) {
     }
     //Custom error
     else if(error && error.msg){
-        component.$root.$emit("ShowError", error.msg);   
-    }
-    //Back-end errors: show entire message
-    else if (error && error.response && error.response.data && error.response.data.message)
-    {
-        component.$root.$emit("ShowError", "Une erreur est survenue, contacter l'administrateur. " + error.response.data.message);   
+        component.$root.$emit("ShowError", `Une erreur est survenue, contacter l'administrateur.\n${error.msg}`);
     }
     //Custom error
     else if(error){
-        component.$root.$emit("ShowError", error);   
+        component.$root.$emit("ShowError", `Une erreur est survenue, contacter l'administrateur.\n${error}`);
     }
     //All other error codes
     else
     {
-       component.$root.$emit("ShowError", "Une erreur est survenue, contacter l'administrateur.");   
+       component.$root.$emit("ShowError", "Une erreur est survenue, contacter l'administrateur.");
     }
 };
